dc8229287bfaa1fb50d59885c4e0ffa6fe0b90cb
Joe Lotz <joelotz@gmail.com>
2021-01-22 19:46:13 +0530

    just tuning

--- pelicanconf.py
+++ pelicanconf.py
@@ -72,28 +72,12 @@ GOOGLE_ANALYTICS_ACCOUNT = 'UA-48351953-1 '
 
 ## PLUGIN CONFIGURATION ##################################
 MARKUP = ('md', 'ipynb')
-"""
-MARKDOWN = {
-    'extension_configs': {
-        'markdown.extensions.extra': {},
-        'markdown.extensions.admonition': {},
-        'markdown.extensions.codehilite': {
-            'css_class': 'highlight'
-        },
-        'markdown.extensions.meta': {},
-        'smarty' : {
-            'smart_angled_quotes' : 'true'
-        },
-        'markdown.extensions.toc': {
-            'permalink': 'true',},
-    }
-}
-"""
 
 # https://jackdewinter.github.io/2019/10/16/fine-tuning-pelican-markdown-configuration/
 MARKDOWN = {
     'extension_configs': {
         'markdown.extensions.extra': {},
+        'markdown.extensions.footnotes': {},
         'markdown.extensions.admonition': {},
         'markdown.extensions.nl2br': {},
         'smarty' : {
